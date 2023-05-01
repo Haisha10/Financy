ea55526502ee3282a985b978153d8d677888a31e
AlMot12 <u20201c280@upc.edu.pe>
2023-04-30 22:19:45 -0500

    enrutado menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { OfferWorkComponent} from './offer-work/offer-work.component'
 import { FinancesComponent } from './finances/finances.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  {path:'offer-work',component: OfferWorkComponent},
+  { path:'offer-work',component: OfferWorkComponent},
   { path: 'finances', component: FinancesComponent },
+  { path:'main-menu',component:MainMenuComponent}
 ];
 
 @NgModule({
