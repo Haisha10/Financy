1b567cc460f27589238680c1ae7fecdd5180eef0
AlvaroL <u202012770@upc.edu.pe>
2023-04-30 23:03:20 -0500

    fix: visuals

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { OfferWorkComponent} from './offer-work/offer-work.component'
+import { OfferWorkComponent } from './offer-work/offer-work.component'
 import { FinancesComponent } from './finances/finances.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { RecommendsComponent } from './recommends/recommends.component';
@@ -13,10 +13,10 @@ const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path:'offer-work',component: OfferWorkComponent},
+  { path: 'offer-work', component: OfferWorkComponent },
   { path: 'finances', component: FinancesComponent },
-  { path:'main-menu',component:MainMenuComponent},
-  { path:'recommends',component:RecommendsComponent}
+  { path: 'menu', component: MainMenuComponent },
+  { path: 'recommends', component: RecommendsComponent }
 ];
 
 @NgModule({
