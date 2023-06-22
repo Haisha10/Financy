94ce72bc526fe2ae01672a084a69b7994f268425
AlvaroL <u202012770@upc.edu.pe>
2023-06-21 19:04:32 -0500

    add: tips format

--- src/app/components/tips/tips.component.ts
+++ src/app/components/tips/tips.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tips',
   templateUrl: './tips.component.html',
-  styleUrls: ['./tips.component.css']
+  styleUrls: ['./tips.component.scss']
 })
 export class TipsComponent {
-
+  redirectToURL() {
+    const url = 'https://www.bbva.mx/educacion-financiera/blog/30-recomendaciones-para-mejorar-tus-finanzas.html';
+    window.open(url, '_blank');
+  }
 }
