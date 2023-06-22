bdfd176d917e1e8946fb623b239b39302a50707c
AlvaroL <u202012770@upc.edu.pe>
2023-06-21 19:23:04 -0500

    test: deploy release

--- src/app/services/environment.ts
+++ src/app/services/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  base_url: 'http://localhost:8080/api/v1',
-  //'https://financyapi-production.up.railway.app/api/v1'
+  base_url: 'https://financyapi-production.up.railway.app/api/v1',
+  //'http://localhost:8080/api/v1'
 };
 
 //'https://my-json-server.typicode.com/Haisha10/Financy'
