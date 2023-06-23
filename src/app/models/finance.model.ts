d474706822fe1f8e21651b2b842d6b4a7fba899c
AlvaroL <u202012770@upc.edu.pe>
2023-06-23 18:48:30 -0500

    Auto-generated commit [ci skip]

--- src/app/models/finance.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Finance {
-  id: number;
-  isIncome: boolean;
-  date: Date;
-  name: String;
-  exchange: number;
-  comment: String;
-}
--- src/app/models/user.model.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  lastname: string;
-  email: string;
-  password: string;
-  isBusiness: boolean;
-}
